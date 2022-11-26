6678efd4473abbd3ceca3fae3a6691b238536ab0
Daniil <gggchaseggg@gmail.com>
2022-11-26 21:02:37 +0300

    add-ServicesInfo-model&controller

---
+++ future-academy/Controllers/ServicesInfoController.cs
@@ -0,0 +1,18 @@
+﻿using future_academy.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace future_academy.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ServicesInfoController : ControllerBase
+    {
+
+        [HttpGet]
+        public ServicesInfo Get()
+        {
+            return new ServicesInfo { id = 1, openDoorsDate = DateTime.Now, programsCount = 1, teachersCount = 2, graduatesCount = 3 };
+
+        }
+    }
+}
---
+++ future-academy/Models/ServicesInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace future_academy.Models
+{
+    public class ServicesInfo
+    {
+        public int id { get; set; }
+        public DateTime openDoorsDate { get; set; }
+        public int programsCount { get; set; }
+        public int teachersCount { get; set; }
+        public int graduatesCount { get; set; }
+    }
+}
